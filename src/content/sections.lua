f5dac2cb97851620ca87532e98dad83d6cb96681
Hugh Leather <hughleat@gmail.com>
2016-07-02 13:45:15 +0100

    Temporarily hiding some sections

--- src/content/sections.lua
+++ src/content/sections.lua
@@ -1,12 +1,12 @@
 sections = {
     { id = "intro", title = "Intro" },
     { id = "prospective", title = "Prospective Students" },
-    { id = "research", title = "Research" },
+    --{ id = "research", title = "Research" },
     { id = "team", title = "Team", built = true },
     { id = "compucast", title = "CompuCast" },
-    { id = "teaching", title = "Teaching" },
+    --{ id = "teaching", title = "Teaching" },
     { id = "pubs", title = "Publications", built = true },
-    { id = "awards", title = "Awards" },
+    --{ id = "awards", title = "Awards" },
     { id = "contact", title = "Contact" },
 }
 
