a2caac0d88b0ce7a6c7e1681ed8aedb1dd0dc565
Hugh Leather <hughleat@gmail.com>
2016-07-03 17:10:32 +0100

    Reordering sections
    Team first

--- src/content/sections.lua
+++ src/content/sections.lua
@@ -1,8 +1,8 @@
 sections = {
     { id = "intro", title = "Intro" },
     { id = "prospective", title = "Prospective Students" },
-    { id = "research", title = "Research" },
     { id = "team", title = "Team", built = true },
+    { id = "research", title = "Research" },
     { id = "compucast", title = "CompuCast" },
     { id = "teaching", title = "Teaching" },
     { id = "pubs", title = "Publications", built = true },
